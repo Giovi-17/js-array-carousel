4074b048b1291d34f9c3d4b97855dea78861211d
Giovi-17 <giovanni.nuzzo17@gmail.com>
2021-11-18 15:32:15 +0100

    MILESTONE 1

---
+++ MILESTONE 1/js/script.js
@@ -0,0 +1,33 @@
+/* Per prima cosa, 
+creiamo il markup statico: 
+costruiamo il container e 
+inseriamo l'immagine grande a sinistra 
+e le thumbnails sulla destra 
+in modo da poter stilare lo slider; 
+avremo così la struttura base 
+e gli stili pronti 
+per poterci poi concentrare solamente sull'aspetto logico. */
+
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
